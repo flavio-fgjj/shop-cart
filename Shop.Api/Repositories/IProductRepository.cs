6838062b0d83ee66751037870de24a29c28afbaa
Flavio A Alvarenga <flavio.fgjj@live.com>
2022-12-17 12:05:37 -0300

    Repository Pattern   - Added Interface and Repository for Product Class

---
+++ Shop.Api/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Shop.Api.Entities;
+
+namespace Shop.Api.Repositories;
+
+public interface IProductRepository
+{
+    // defining contracts for Product and these contracts should be implemented by who will use
+    Task<IEnumerable<Product>> GetItems();
+    Task<Product> GetItem(int id);
+    Task<IEnumerable<Product>> GetItemsByCategory(int id);
+
+}
---
+++ Shop.Api/Repositories/ProductRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shop.Api.Context;
+using Shop.Api.Entities;
+
+namespace Shop.Api.Repositories;
+
+public class ProductRepository : IProductRepository
+{
+    // This class will access the database 
+
+    private readonly AppDbContext _context; // dependency injection
+    public ProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Product> GetItem(int id)
+    {
+        var product = await _context.Products
+                        .Include(c => c.Category)            
+                        .FirstOrDefaultAsync(c => c.Id == id);
+
+        return product;
+    }
+
+    public async Task<IEnumerable<Product>> GetItems()
+    {
+        var products = await _context.Products
+                        .Include(c => c.Category)
+                        .ToListAsync();
+
+        return products;
+    }
+
+    public async Task<IEnumerable<Product>> GetItemsByCategory(int id)
+    {
+        var productsByCategory = await _context.Products
+                        .Include(c => c.Category)
+                        .Where(c => c.CategoryId == id)
+                        .ToListAsync();
+
+        return productsByCategory;
+    }
+}
