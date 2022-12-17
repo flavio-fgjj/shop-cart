792bf62e9c1e4234bbc14e86a7d78f716cd418ac
Flavio A Alvarenga <flavio.fgjj@live.com>
2022-12-17 12:09:26 -0300

    registering the services on the container DI (program.cs)  - AddScoped for IProductRepository

--- Shop.Api/Program.cs
+++ Shop.Api/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using Shop.Api.Context;
+using Shop.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +14,9 @@ builder.Services.AddSwaggerGen();
 // Define the context service (always before builder.Build())
 var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
+
+// registering the services on the container DI
+builder.Services.AddScoped<IProductRepository, ProductRepository>(); // It gives an instance for each reference for IProductRepository
 
 var app = builder.Build();
 
