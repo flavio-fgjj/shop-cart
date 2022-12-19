a23ce67f0495c6c7e3ae14f9e0a85a2129d07286
Flavio A Alvarenga <flavio.fgjj@live.com>
2022-12-19 17:06:37 -0300

    new unit test added

--- Shop.Test/WebTest.cs
+++ Shop.Test/WebTest.cs
@@ -2,39 +2,49 @@ using Bunit;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
-using Shop.Models.DTOs;
-using Shop.Web.Pages.Components.Cart;
-using Shop.Web.Pages.Components.Products;
+using Shop.Web.Pages;
 using Shop.Web.Services;
 using Shop.Web.Shared;
 using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
 using Xunit;
 
 namespace Shop.Test
 {
     public class WebTest : TestContext
     {
+        //[Fact]
+        //public void RenderHyperlinks()
+        //{
+        //    var cartBuyServiceMoq = new Mock<ICartBuyService>();
+        //    var productsServiceMoq = new Mock<IProductService>();
+
+        //    Services.AddSingleton<ICartBuyService>(cartBuyServiceMoq.Object);
+        //    Services.AddSingleton<IProductService>(productsServiceMoq.Object);
+
+        //    // act
+        //    var cut = RenderComponent<ProductsCategoryNavMenu>();
+
+        //    cut.Find("#menu_Beleza").Click();
+
+
+        //    var nav = Services.GetRequiredService<NavigationManager>();
+
+        //    // assert
+        //    Assert.Equal("https://localhost:7000/ProductsByCategory/1", nav.Uri);
+
+        //}
+
         [Fact]
-        public void RenderHyperlinks()
+        public void RenderimageLogo()
         {
-            var cartBuyServiceMoq = new Mock<ICartBuyService>();
-            var productsServiceMoq = new Mock<IProductService>();
-
-            Services.AddSingleton<ICartBuyService>(cartBuyServiceMoq.Object);
-            Services.AddSingleton<IProductService>(productsServiceMoq.Object);
-
             // act
-            var cut = RenderComponent<ProductsCategoryNavMenu>();
-
-            cut.Find("#menu_Beleza").Click();
-
-
-            var nav = Services.GetRequiredService<NavigationManager>();
+            var cut = RenderComponent<Web.Pages.Index>();
+            //cut.Find("#image-logo");
 
             // assert
-            Assert.Equal("https://localhost:7000/ProductsByCategory/1", nav.Uri);
+            var expectedMarkup = @"<div class=""text-center"" ><img id=""image-logo"" src=""/shopping-cart2.jpg"" class=""rounded"" asp-append-version=""true"" alt=""Shopping Cart"" ></div>";
+
+            cut.MarkupMatches(expectedMarkup);
 
         }
     }
